a62dac677d06c736c6600eacbc4d888a6d854107
Kush <kush.patel280703@gmail.com>
2023-10-30 18:27:53 +0530

    Footer component

--- Project/frontend/src/components/App.jsx
+++ Project/frontend/src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Login from "./Login";
 import NgoSignIn from "./NgoSignIn";
 import NgoRegistration from "./NgoRegistration";
+import Footer from "./footer";
+import "../Styles/styles.css";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App()
 {
@@ -10,6 +12,7 @@ function App()
         <Route path="/" element={<Login />}/>
         <Route path="/ngosignin" element={<NgoSignIn />}/>
         <Route path="/Registration" element={<NgoRegistration />}/>
+        <Route path="/footer" element={<Footer />}/>
   </Routes> 
   </Router>
 }
