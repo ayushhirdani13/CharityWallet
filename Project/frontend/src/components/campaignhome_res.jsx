9effca511ff0a39c4523e2ec075ca5365cca4d96
Darshan <darshansanghavi01@gmai.com>
2023-11-25 00:41:19 +0530

    Emergency Fundraiser_Dashboard Navbar CampaignHome commited

---
+++ Project/frontend/src/components/campaignhome_res.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import '../Styles/campaignhome_res.css';
+function Campaignhome(){
+    return(
+        <div class="container-fluid">
+        
+        <div class="container px-5"> 
+           
+            <h1 class="fw-bold text-center hsize33 py-4 ">Explore Campaigns</h1>
+            
+            <input type="search" class="form-control rounded-4 border border-dark my-3" placeholder="Search..." aria-label="Search"/> 
+
+            
+            
+            
+            <div class="row py-4">
+                <div class="col-lg-6 col-12">
+                    <img src="https://picsum.photos/1600/900" class="img-fluid border rounded-3 shadow-lg " alt="Example image" loading="lazy"/>
+                    <h1 class="text-center fw-bold fs-2 mt-2">Sankalp NGOs</h1>
+                    <div class="concolor33 py-1 px-1  rounded-4 text-center"> 
+                        <p class="mt-2 fs-5 fw-normal">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <img src="https://picsum.photos/1600/900" class="img-fluid border rounded-3 shadow-lg " alt="Example image" loading="lazy"/>
+                    <h1 class="text-center fw-bold fs-2 mt-2">Sankalp NGOs</h1>
+                    <div class="concolor33 py-1 px-1  rounded-4 text-center"> 
+                        <p class="mt-2 fs-5 fw-normal">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row py-4">
+                <div class="col-lg-6 col-12">
+                    <img src="https://picsum.photos/1600/900" class="img-fluid border rounded-3 shadow-lg " alt="Example image" loading="lazy"/>
+                    <h1 class="text-center fw-bold fs-2 mt-2">Sankalp NGOs</h1>
+                    <div class="concolor33 py-1 px-1  rounded-4 text-center"> 
+                        <p class="mt-2 fs-5 fw-normal">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <img src="https://picsum.photos/1600/900" class="img-fluid border rounded-3 shadow-lg " alt="Example image" loading="lazy"/>
+                    <h1 class="text-center fw-bold fs-2 mt-2">Sankalp NGOs</h1>
+                    <div class="concolor33 py-1 px-1  rounded-4 text-center"> 
+                        <p class="mt-2 fs-5 fw-normal">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    );
+}
+ export default Campaignhome;
