74b9a5d1ea77e510602bc88be4aed930a126ed2d
ayushhirdani13 <ayushhirdani13@gmail.com>
2023-11-30 14:41:43 +0530

    Image Hanlding Changes in API

--- Project/backend/middlewares/imageHandler.js
+++ Project/backend/middlewares/imageHandler.js
@@ -1,30 +1,15 @@
 import multer from "multer";
-import fs from "fs";
 import { drive } from "../app.js";
-import path from "path";
 import sharp from "sharp";
 
-const uploadsDir = path.join(path.resolve(), "uploads");
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
-}
+const uploadsDir = "../uploads";
 
-const Storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Set the destination folder to the user's subdirectory
-    cb(null, uploadsDir);
-  },
-  filename: function (req, file, cb) {
-    // Set the file name
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const Storage = multer.memoryStorage()
 
 export const upload = multer({ storage: Storage });
 
 export const uploadLogoGdrive = async (file, next) => {
   try {
-    const filePath = file.path;
     const response = await drive.files.create({
       requestBody: {
         name: file.filename,
@@ -33,11 +18,9 @@ export const uploadLogoGdrive = async (file, next) => {
       },
       media: {
         mimeType: "image/jpeg",
-        body: fs.createReadStream(filePath),
+        body: file.buffer,
       },
     });
-
-    await fs.promises.unlink(filePath);
 
     return response.data.id;
   } catch (error) {
@@ -48,16 +31,13 @@ export const uploadLogoGdrive = async (file, next) => {
 
 export const updateLogoGdrive = async (fileId, file, next) => {
   try {
-    const filePath = file.path;
     const response = await drive.files.update({
       fileId: fileId,
       media: {
         mimeType: "image/jpeg",
-        body: fs.createReadStream(filePath),
+        body: file.buffer,
       },
     });
-
-    await fs.promises.unlink(filePath);
 
     return response.data.id;
   } catch (error) {
@@ -115,15 +95,12 @@ export const uploadMultipleImagesGdrive = async (files, next) => {
           },
           media: {
             mimeType: "image/jpeg",
-            body: fs.createReadStream(filePath),
+            body: file.buffer,
           },
         });
 
         // Save the uploaded image ID
         uploadedImageIds.push(response.data.id);
-
-        // Delete the temporary file
-        await fs.promises.unlink(filePath);
       })
     );
 
